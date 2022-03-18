3b783198085fac363708b63a2f3a9eee522c1d1d
Cleber Dias <md.cleber@gmail.com>
2022-03-18 18:17:57 +0100

    Initital commit

---
+++ WiredBrainCoffeee/Pages/Item.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using WiredBrainCoffee.Models;
+using WiredBrainCoffee.Services;
+
+namespace WiredBrainCoffee.Pages
+{
+    public class ItemModel : PageModel
+    {
+        public MenuItem Item { get; set; }
+
+        public void OnGet(int id)
+        {
+            var menuService = new MenuService();
+            Item = menuService.GetMenuItems().FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
---
+++ WiredBrainCoffeee/Services/IMenuService.cs
@@ -0,0 +1,9 @@
+﻿using WiredBrainCoffee.Models;
+
+namespace WiredBrainCoffee.Services
+{
+    public interface IMenuService
+    {
+        List<MenuItem> GetMenuItems();
+    }
+}
